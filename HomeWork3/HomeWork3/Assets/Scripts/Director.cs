190c4b333cf38a3e7b0f179fc7819ecb99bfd5b6
charlieleex <d1447926196@163.com>
2018-04-02 14:25:03 +0000

    push

---
+++ HomeWork3/HomeWork3/Assets/Scripts/Director.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace PAD
+{
+    class Director: System.Object
+    {
+        private static Director _instance;
+        public SceneController current { set; get; }
+
+        public static Director getInstance()
+        {
+            if(_instance==null)
+                _instance=new Director();
+            return _instance;
+        }
+    }
+}
