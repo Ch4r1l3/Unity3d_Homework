21445dcd58107f1c0dae35caf83f4c8f05eb054e
charlieleex <d1447926196@163.com>
2018-04-09 23:11:57 +0800

    push

---
+++ HomeWork3/HomeWork3/Assets/Scripts/CCActionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PAD
+{
+    public class CCActionManager : SSActionManager, ISSActionCallBack
+    {
+        public FirstController sceneController;
+
+        public void moveBoat(GameObject boat, Vector3 target)
+        {
+            CCBoatMove boatMoveToLeft = CCBoatMove.GetSSAction(target);
+            this.RunAction(boat, boatMoveToLeft, this);
+        }
+
+        public void moveCharacter(GameObject obj, Vector3 dest)
+        {
+            CCCharacterMove characterMove = CCCharacterMove.GetSSAction(dest);
+            this.RunAction(obj, characterMove, this);
+        }
+        
+
+        protected new void Update()
+        {
+            base.Update();
+        }
+
+        public void SSActionEvent(SSAction sourse, SSActionEventType events = SSActionEventType.Completed)
+        {
+        }
+    }
+}
