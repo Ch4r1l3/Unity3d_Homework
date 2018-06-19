a3de8bb6cf6ea737771199132dfdd914ebd60979
charlieleex <d1447926196@163.com>
2018-06-19 14:12:07 +0800

    push

---
+++ Assets/AI.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChobiAssets.KTP;
+
+[RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+public class AI : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform player;
+    public ID_Control_CS idScript;
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+    public Vector3[] partol = {new Vector3(-60,2,128),new Vector3(60,2,128),new Vector3(60,2,-128),new Vector3(-60,2,-128)};
+    int idx = 0;
+    bool isChase = false;
+    void Start () {
+        agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+        agent.stoppingDistance = 5.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //agent.SetDestination(player.position);
+
+        var direction = player.transform.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+        
+        if(angle<55)
+        {
+            RaycastHit hit;
+            var tmp_dire = player.position- transform.position;
+            if(Physics.Raycast(transform.position, tmp_dire.normalized ,out hit,1000))
+            {
+                if (player.IsChildOf(hit.collider.gameObject.transform))
+                {
+                    isChase = true;
+                }
+            }
+        }
+        
+
+        if(!isChase)
+        {
+            agent.SetDestination(partol[idx % 4]);
+
+            var speed = Vector3.Project(agent.desiredVelocity, transform.forward).magnitude;
+            transform.LookAt(transform.position + agent.desiredVelocity);
+
+            var wheel = gameObject.GetComponent<Wheel_Control_CS>();
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                wheel.leftRate = -0.5f;
+                wheel.rightRate = 0.5f;
+            }
+
+            if (Vector3.Distance(transform.position, partol[idx % 4]) < 10)
+                idx++;
+        }
+        else
+        {
+            if(Vector3.Distance(transform.position,player.position)>100)
+            {
+
+                agent.SetDestination(player.transform.position);
+
+                transform.LookAt(transform.position + agent.desiredVelocity);
+                var wheel = gameObject.GetComponent<Wheel_Control_CS>();
+                if (agent.remainingDistance > agent.stoppingDistance)
+                {
+                    wheel.leftRate = -0.5f;
+                    wheel.rightRate = 0.5f;
+                }
+            }
+            else
+            {
+                var wheel = gameObject.GetComponent<Wheel_Control_CS>();
+                if(gameObject.GetComponent<Rigidbody>().velocity.magnitude>3)
+                {
+
+                    wheel.leftRate = 0.5f;
+                    wheel.rightRate = -0.5f;
+                }
+                else
+                {
+                    wheel.leftRate = 0f;
+                    wheel.rightRate = 0f;
+                }
+            }
+            var turret = gameObject.GetComponentInChildren<Turret_Control_CS>();
+            turret.targetTransform = player;
+            turret.targetPos = player.position;
+            turret.targetOffset.y = 0.5f;
+            turret.isTracking = true;
+            
+            idScript.fireButton = true;
+        }
+        
+	}
+}
---
+++ Assets/Kawaii_Tanks_Project/Kawaii_Tanks_Assets/Scripts/ID_Control_CS.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+// This script must be attached to the top object of the tank.
+namespace ChobiAssets.KTP
+{
+
+	public class ID_Control_CS : MonoBehaviour
+	{
+
+		[Header ("ID settings")]
+		[Tooltip ("ID number")] public int id = 0;
+
+		[HideInInspector] public bool isPlayer; // Referred to from child objects.
+		[HideInInspector] public Game_Controller_CS controllerScript;
+		[HideInInspector] public TankProp storedTankProp; // Set by "Game_Controller_CS".
+
+		[HideInInspector] public Turret_Control_CS turretScript;
+		[HideInInspector] public Camera_Zoom_CS mainCamScript;
+		[HideInInspector] public GunCamera_Control_CS gunCamScript;
+
+
+		void Start ()
+		{ // Do not change to "Awake ()".
+			// Send this reference to the "Game_Controller" in the scene.
+			GameObject gameController = GameObject.FindGameObjectWithTag ("GameController");
+			if (gameController) {
+				controllerScript = gameController.GetComponent <Game_Controller_CS> ();
+			}
+			if (controllerScript) {
+				controllerScript.Receive_ID (this);
+			} else {
+				Debug.LogError ("There is no 'Game_Controller' in the scene.");
+			}
+			// Broadcast this reference.
+			BroadcastMessage ("Get_ID_Script", this, SendMessageOptions.DontRequireReceiver);
+		}
+
+		#if !UNITY_ANDROID && !UNITY_IPHONE
+		[HideInInspector] public bool aimButton;
+		[HideInInspector] public bool aimButtonDown;
+		[HideInInspector] public bool aimButtonUp;
+		[HideInInspector] public bool dragButton;
+		[HideInInspector] public bool dragButtonDown;
+		[HideInInspector] public bool fireButton;
+		void Update ()
+		{
+			if (isPlayer) {
+				aimButton = Input.GetKey (KeyCode.Space);
+				aimButtonDown = Input.GetKeyDown (KeyCode.Space);
+				aimButtonUp = Input.GetKeyUp (KeyCode.Space);
+				dragButton = Input.GetMouseButton (1);
+				dragButtonDown = Input.GetMouseButtonDown (1);
+				fireButton = Input.GetMouseButton (0);
+			}
+		}
+		#endif
+
+		void Destroy ()
+		{ // Called from "Damage_Control_CS".
+			gameObject.tag = "Finish";
+		}
+
+		public void Get_Current_ID (int currentID)
+		{ // Called from "Game_Controller_CS".
+			if (id == currentID) {
+				isPlayer = true;
+			} else {
+				isPlayer = false;
+			}
+			// Call Switch_Player.
+			turretScript.Switch_Player (isPlayer);
+			mainCamScript.Switch_Player (isPlayer);
+			gunCamScript.Switch_Player (isPlayer);
+		}
+
+	}
+
+}
